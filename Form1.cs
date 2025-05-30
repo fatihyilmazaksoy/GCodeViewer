9e4d74a3856cbdd8e86c7306963d7ddcda94b3c4
fatihyilmazaksoy <fatihyilmazaksoy@outlook.com>
2025-05-30 21:42:57 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace GCodeViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
